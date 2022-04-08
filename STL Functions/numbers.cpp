180559e23b93ab400e2f096c463dc2ed0ad7535c
shubham-up-47 <91260340+shubham-up-47@users.noreply.github.com>
2022-04-08 15:38:43 +0530

    Add files via upload

---
+++ STL Functions/numbers.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ 
+int main()
+{ 
+	cout<< __gcd(10,6) <<endl;                      // gcd
+	cout<< (10*6)/__gcd(10,6) <<endl;               // lcm 
+	
+	cout<< pow(3,2) <<endl;                         // power
+	cout<< pow(2.2,-5.8) <<endl;
+	
+	cout<< min(17,43) <<endl;                       // minimum 
+	cout<< max(17,43) <<endl;                       // maximum  
+
+	int x=10, y=12;
+	swap(x,y);                                      // swapping/interchanging
+	cout<<x<<" "<<y<<endl; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
